fcf8458463bf282fa29f2d65a29b465eddb1af32
Luca <cecere.luca.cecere@gmail.com>
2026-04-17 13:46:36 -0500

    Smaller charge current

--- src/eTapeLogger02.ino
+++ src/eTapeLogger02.ino
@@ -217,7 +217,7 @@ void battSettings() {
   SystemPowerConfiguration conf; 
   conf.powerSourceMaxCurrent(900)    // 5W / 5V = 1000mA. 900mA is the closest PMIC register setting.
       .powerSourceMinVoltage(3880)  
-      .batteryChargeCurrent(500)
+      .batteryChargeCurrent(150)
       .batteryChargeVoltage(4110);  
       
   System.setPowerConfiguration(conf);
